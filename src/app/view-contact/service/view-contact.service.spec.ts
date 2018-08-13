a92c7844f7f8d1e4e947c274cc09776d05779375
rmeghlan <rmeghlan@wdtablesystems.com>
2018-08-13 13:44:22 +0530

    Added test cases for view-contact.service

--- src/app/view-contact/service/view-contact.service.spec.ts
+++ src/app/view-contact/service/view-contact.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { ViewContactService } from './view-contact.service';
+import {ViewContactService} from './view-contact.service';
 
 describe('ViewContactService', () => {
   beforeEach(() => {
@@ -11,5 +11,20 @@ describe('ViewContactService', () => {
 
   it('should be created', inject([ViewContactService], (service: ViewContactService) => {
     expect(service).toBeTruthy();
+  }));
+
+  it('should save the filter to localstorage', inject([ViewContactService], (service: ViewContactService) => {
+    const filters = 'abc';
+    service.saveFilter(filters);
+    expect(JSON.parse(localStorage.getItem('contactInfo')).filters).toEqual(filters);
+  }));
+
+  it('should save the sortData to localstorage', inject([ViewContactService], (service: ViewContactService) => {
+    const sortObj = {
+      direction: 'asc',
+      active: 'firstName'
+    };
+    service.saveSortData(sortObj);
+    expect(JSON.parse(localStorage.getItem('contactInfo')).sort).toEqual(sortObj);
   }));
 });
