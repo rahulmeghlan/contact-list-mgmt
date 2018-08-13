2463b54498c05e9214af697fad1799063857ba23
rmeghlan <rmeghlan@wdtablesystems.com>
2018-08-13 12:15:18 +0530

    Dependency injection in add-contact.component.spec to make the component available.

--- src/app/add-contact/component/add-contact.component.spec.ts
+++ src/app/add-contact/component/add-contact.component.spec.ts
@@ -1,6 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AddContactComponent } from './add-contact.component';
+import {AddContactComponent} from './add-contact.component';
+import {MatButtonModule, MatFormFieldModule, MatInputModule, MatLabel} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {RouterTestingModule} from '@angular/router/testing';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 describe('AddContactComponent', () => {
   let component: AddContactComponent;
@@ -8,9 +13,12 @@ describe('AddContactComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddContactComponent ]
+      imports: [
+        MatFormFieldModule, MatInputModule, MatButtonModule,
+        FormsModule, ReactiveFormsModule, CommonModule, RouterTestingModule, BrowserAnimationsModule],
+      declarations: [AddContactComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
